a669ecf7c62e18af1677708435d52ff697100c73
wanghaisong <wanghaisong@huoyunren.com>
2016-09-14 18:48:45 +0800

    提交

---
+++ text/index.py
@@ -0,0 +1,20 @@
+flag = False
+name = 'luren'                          #路人
+if name == 'python':
+    flag = True
+    print('welcome boss')
+else:
+    print(name)
+    var = 100
+
+if ( var == 100 ) : print("变量 var 的值为100")
+print("Good bye!")
+
+
+value = 0
+for num in range(1,101):
+
+    value += num
+    print(value)
+
+print(value)
